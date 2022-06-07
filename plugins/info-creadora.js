e0489caa62e5aac84b2028e08a835174dd6e6d39
imZemo <106836635+imZemo@users.noreply.github.com>
2022-06-07 01:28:39 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -2,16 +2,18 @@ import fs from 'fs'
 function handler(m, { conn }) {
 let text = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
-*Wa.me/593993684821 (BOT)*
-*Wa.me/51993042301 (BOT)*
-*https://www.instagram.com/gata_dios*
+*Wa.me/51917611323 (Owner)*
+*Wa.me/51993042301 (Sub - Owner)*
+*Wa.me/51933462225 (Moderador)
+*Wa.me/51991228197 (Sub - Bot aun no funcional)
+*https://www.instagram.com/destroyerofc._/*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+title: '𝐔𝐧 𝐒𝐢𝐦𝐩𝐥𝐞 𝐁𝐨𝐭𝐬𝐢𝐭𝐨 𓃵',
 body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://wa.me/593993684821`}}})
+sourceUrl: `https://wa.me/51917611323`}}})
   
 //const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 //this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
@@ -19,7 +21,7 @@ sourceUrl: `https://wa.me/593993684821`}}})
 let pp = './media/menus/Menu2.jpg'
 let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
 
-conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/gata_dios', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/destroyerofc._/', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
 ['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
 ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
